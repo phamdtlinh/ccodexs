668e158b8a6aee37be90364060e3699f52865741
Linh Pham <linhphotos01@gmail.com>
2026-03-10 23:54:48 +0700

    update tree.c

--- simple/tree/tree.c
+++ simple/tree/tree.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <limits.h>
 
 typedef struct TreeNode TreeNode;
 
@@ -14,41 +16,41 @@ struct TreeNode {
 TreeNode *tree_node_insert(TreeNode *treenode, int value);
 TreeNode *tree_node_search(TreeNode *treenode, int value);
 TreeNode *tree_node_delete(TreeNode *treenode, int value);
+int tree_find_max(TreeNode *treenode);
+int tree_find_min(TreeNode *treenode);
 void tree_print_nodes(TreeNode *treenode);
 
+// call seeding() before use rig011 and rig012
+void timespec_seeding();
+int rig011(int min, int max);
+int rig012(int min, int max);
+
 int main(int argc, char **argv) {
+	timespec_seeding();
 	TreeNode *root = NULL;
-	root = tree_node_insert(root, 50);
-	root = tree_node_insert(root, 25);
-	root = tree_node_insert(root, 75);
-	root = tree_node_insert(root, 12);
-	root = tree_node_insert(root, 37);
-	root = tree_node_insert(root, 62);
-	root = tree_node_insert(root, 87);
-	root = tree_node_insert(root, 6);
-	root = tree_node_insert(root, 18);
-	root = tree_node_insert(root, 31);
-	root = tree_node_insert(root, 43);
-	root = tree_node_insert(root, 56);
-	root = tree_node_insert(root, 68);
-	root = tree_node_insert(root, 81);
-	root = tree_node_insert(root, 93);
-	root = tree_node_insert(root, 3);
-	root = tree_node_insert(root, 9);
-	root = tree_node_insert(root, 21);
-	root = tree_node_insert(root, 34);
-	root = tree_node_insert(root, 40);
-	TreeNode *deletedParent = tree_node_delete(root, 0);
-	if (deletedParent) {
-		printf("Deleted value: %d\n", deletedParent->data);
-	} else {
-		printf("Not found to delete\n");
+	int max_capacity = 50;
+	root = tree_node_insert(root, max_capacity);
+	if (root) {
+		for (int i=1; i <= max_capacity; i++) {
+			int value = rig011(0,max_capacity * 2);
+			if (tree_node_search(root, value)) continue;
+			printf("%d", value);
+			if (i<max_capacity) printf(", ");
+			root = tree_node_insert(root, value);
+		}
+		printf("\n");
+		tree_print_nodes(root);
+		printf("Min: %d\n", tree_find_min(root));
+		printf("Max: %d\n", tree_find_max(root));
+		root = tree_node_delete(root, 20);
+		printf("Root value: %d\n", root->data);
+		tree_print_nodes(root);
 	}
-	tree_print_nodes(root);
 	
 }
 
 TreeNode *tree_node_insert(TreeNode *treenode, int value) {
+	if (tree_node_search(treenode, value)) return treenode;
 	if (!treenode) {
 		TreeNode *tn = malloc(sizeof(*tn));
 		if (!tn) {
@@ -90,11 +92,43 @@ TreeNode *tree_node_delete(TreeNode *treenode, int value) {
 	} else if (treenode->data < value) {
 		treenode->right = tree_node_delete(treenode->right, value);
 	} else {
-		// DO SOMETHING HERE
+		if (!treenode->left) { 
+			TreeNode* temp = treenode->right;
+			free(treenode);
+			return temp; 
+		}
+		if (!treenode->right) { 
+			TreeNode* temp = treenode->left;
+			free(treenode);
+			return temp; 
+		}
+		int rep_side = rig011(0,1) % 2; // 1 = max of left, 0 = min of right
+		if (rep_side) {
+			treenode->data = tree_find_max(treenode->left);
+    		treenode->left = tree_node_delete(treenode->left, treenode->data);
+		} else {
+			treenode->data = tree_find_min(treenode->right);
+    		treenode->right = tree_node_delete(treenode->right, treenode->data);
+		}
+		printf("Deleted %d\n", value);
 	}
+	
 	return treenode;
 }
-
+int tree_find_max(TreeNode *treenode) {
+	if (!treenode) {
+		return 0;
+	}
+	while(treenode->right) treenode = treenode->right;
+	return treenode->data;
+}
+int tree_find_min(TreeNode *treenode) {
+	if (!treenode) {
+		return 0;
+	}
+	while(treenode->left) treenode = treenode->left;
+	return treenode->data;
+}
 void tree_print_nodes(TreeNode *treenode) {
 	if (!treenode) {
 		fprintf(stdout, "tree_print_nodes treenode nulled!\n");
@@ -107,4 +141,28 @@ void tree_print_nodes(TreeNode *treenode) {
 	if (treenode->right) {
 		tree_print_nodes(treenode->right);
 	}
+}
+
+void timespec_seeding() {
+	struct timespec ts;
+	timespec_get(&ts, TIME_UTC);
+	srand((unsigned int)(ts.tv_nsec ^ ts.tv_sec)); 
+}
+int rig011(int min, int max) {
+	if (min>max) return 0;
+	long long check_point_count = (long long)max - (long long)min + 1;
+	long long b1 = (long long)rand();
+	long long b2 = (long long)rand();
+	return 	(int)((unsigned long long)((b1 << 16) ^ b2) % check_point_count + min);
+}
+int rig012(int min, int max) {
+	if (min>max) return 0;
+	long long check_point_count = (long long)max - (long long)min + 1;
+	long long x1 = (long long)rand();
+	long long x2 = (long long)rand();
+	long long x3 = (long long)rand();
+	x1 ^= x2 << 13;
+	x2 ^= x3 >> 17;
+	x3 ^= x1 << 5;
+	return (int)((unsigned long long)((x1 << 16) ^ (x2 << 8) ^ x3) % check_point_count + min);
 }
