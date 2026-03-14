883b0b342d59d13a183b120370761645b01efc48
Linh Pham <linhphotos01@gmail.com>
2026-03-14 02:52:27 +0000

    fix result_buffer malloc in sarw.c

--- exercises/beginner/02_sarw/sarw.c
+++ exercises/beginner/02_sarw/sarw.c
@@ -22,6 +22,8 @@ int main(int argc, char **argv) {
 	size_t line_buffer_capacity = LINE_BUFFER_INIT_CAPACITY;
 	char *line_buffer = NULL;
 	size_t buff_index = 0;
+	size_t sslen = strlen(argv[2]);
+	size_t rslen = strlen(argv[3]);
 	if (argc < 4) {
 		fprintf(stderr, "Usage: %s <filename> <search> <replace>\n", argv[0]);
 		return 1;
@@ -47,7 +49,9 @@ int main(int argc, char **argv) {
 			if (!tmp) {
 				fprintf(stderr, "line_buffer realloc failed\n");
 				free(line_buffer);
+				fclose(fp);
 				line_buffer = NULL;
+				fp = NULL;
 				return 1;
 			}
 			line_buffer = tmp;
@@ -63,21 +67,26 @@ int main(int argc, char **argv) {
 			int found_count = 0;
 			while ((p = strstr(p, argv[2]))) {
 				found_count++;
-				p += strlen(argv[2]);
+				p += sslen;
 			}
 			if (found_count) {
 				size_t result_capacity = buff_index + 1;
-				int sr_diff = strlen(argv[3]) - strlen(argv[2]);
+				int sr_diff = rslen - sslen;
 				if (sr_diff > 0) {
 					result_capacity += sr_diff*found_count;
 				}
 				char *result_buffer = malloc(result_capacity);
-				// TODO: logic replace here, above this code is fine
+				if (!result_buffer) {
+					fprintf(stderr, "result_buffer malloc failed\n");
+					free(line_buffer);
+					fclose(fp);
+					line_buffer = NULL;
+					fp = NULL;
+					return 1;
+				}
 				char *lbp1 = line_buffer; //lb = line buffer
 				char *lbp2 = line_buffer;
 				char *rbp1 = result_buffer; // rb = result buffer
-				size_t sslen = strlen(argv[2]);
-				size_t rslen = strlen(argv[3]);
 				while ((lbp2 = strstr(lbp2, argv[2]))) {
 					size_t copy_size = lbp2 - lbp1;
 					rbp1 = memcpy(rbp1, lbp1, copy_size);
@@ -88,7 +97,6 @@ int main(int argc, char **argv) {
 					lbp1 = lbp2;
 				}
 				strcpy(rbp1, lbp1);
-				// END HERE below this code is fine
 				printf("Found \"%s\" %d times in below string (need to replace):\n%s",
 						argv[2], found_count, line_buffer);
 				printf("Replaced:\n%s\n\n", result_buffer);
