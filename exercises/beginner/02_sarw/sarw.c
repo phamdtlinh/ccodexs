ca1f2ff6ea8c877e0acabf769e95853bdf5d2d70
Linh Pham <linhphotos01@gmail.com>
2026-03-14 02:00:30 +0000

    finish sarw.c

--- exercises/beginner/02_sarw/sarw.c
+++ exercises/beginner/02_sarw/sarw.c
@@ -66,8 +66,34 @@ int main(int argc, char **argv) {
 				p += strlen(argv[2]);
 			}
 			if (found_count) {
-				// TODO: logic replace here
-				printf("Found \"%s\" %d times in below string (need to replace):\n%s\n", argv[2], found_count, line_buffer);
+				size_t result_capacity = buff_index + 1;
+				int sr_diff = strlen(argv[3]) - strlen(argv[2]);
+				if (sr_diff > 0) {
+					result_capacity += sr_diff*found_count;
+				}
+				char *result_buffer = malloc(result_capacity);
+				// TODO: logic replace here, above this code is fine
+				char *lbp1 = line_buffer; //lb = line buffer
+				char *lbp2 = line_buffer;
+				char *rbp1 = result_buffer; // rb = result buffer
+				size_t sslen = strlen(argv[2]);
+				size_t rslen = strlen(argv[3]);
+				while ((lbp2 = strstr(lbp2, argv[2]))) {
+					size_t copy_size = lbp2 - lbp1;
+					rbp1 = memcpy(rbp1, lbp1, copy_size);
+					rbp1 += copy_size;
+					rbp1 = memcpy(rbp1, argv[3], rslen);
+					rbp1 += rslen;
+					lbp2 += sslen;
+					lbp1 = lbp2;
+				}
+				strcpy(rbp1, lbp1);
+				// END HERE below this code is fine
+				printf("Found \"%s\" %d times in below string (need to replace):\n%s",
+						argv[2], found_count, line_buffer);
+				printf("Replaced:\n%s\n\n", result_buffer);
+				free(result_buffer);
+				result_buffer = NULL;
 			}
 			buff_index = 0;
 		}
@@ -78,8 +104,8 @@ int main(int argc, char **argv) {
 	}
 
 	free(line_buffer);
-	line_buffer = NULL;
 	fclose(fp);
+	line_buffer = NULL;
 	fp = NULL;
 	return 0;
 }
