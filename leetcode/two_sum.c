9064b91ddb11ab901ec149910657609f9ee6f4e1
Linh Pham <linhphotos01@gmail.com>
2026-03-20 04:49:37 +0000

    uncompleted two_sum.c

--- leetcode/two_sum.c
+++ leetcode/two_sum.c
@@ -1,32 +1,82 @@
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TABLE_SIZE 20003
+
+typedef struct {
+	int key;
+	int value;
+	int occupied;
+} Node;
+
+Node hash_table[TABLE_SIZE] = {{0, 0, 0}};
 int *twoSum(int *nums, int numsSize, int target, int *returnSize);
-unsigned int hash(int key, int size);
+int get_hash(int key);
+/*
+void insert(int key, int val);
+int find(int key);
+*/
+
 
 int main(void) {
-    int numsSize = 10;
-    int nums[numsSize] = {1, 7, 2, 4, 6, 3, 1, 5, 9, 8};
-    int max = nums[0];
-    for (int i = 1; i < numsSize; i++) {
-        if (max < nums[i]) max = nums[i];
-    }
-    int ht[max] = {-1};
-    for (int i = 0; i < numsSize; i++) {
-        ht[hash(nums[i], numsSize)] = i;
-    }
-    return 0;
-}
-
-unsigned int hash(int key, int size) {
-    return key % size;
+	int arr[] = {2, 7, 11, 15, 2, 15};
+	int returnSize = 0;
+	int *result = twoSum(arr, 6, 9, &returnSize);
+	for (int i = 0; i < returnSize; i++) {
+		printf("%d\n", result[i]);
+	}
+	return 0;
 }
 
 int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
-    int max = nums[0];
-    for (int i = 1; i < numsSize; i++) {
-        if (max < nums[i]) max = nums[i];
-    }
-    int ht[max] = {-1};
-    for (int i = 0; i < numsSize; i++) {
-        ht[hash(nums[i], numsSize)] = i;
-    }
+	for (int i = 0; i < numsSize; i++) {
+		int compliment = target - nums[i];
+		int hash = get_hash(nums[i]);
+		printf("%d\n", hash);
+		printf("key: %d, value %d, occupied: %d\n", hash_table[hash].key, hash_table[hash].value, hash_table[hash].occupied);
+		if (hash_table[get_hash(compliment)].occupied == 1 ) {
+			int *result = malloc(2 * sizeof(*result));
+			if (!result) {
+				returnSize = 0;
+				return NULL;
+			}
+			result[0] = hash_table[get_hash(compliment)].value;
+			result[1] = i;
+			*returnSize = 2;
+			return result;
+		}
+		while (hash_table[hash].occupied) {
+			hash = (hash + 1) % TABLE_SIZE;
+			hash_table[hash].key = nums[i];
+			hash_table[hash].value = i;
+			hash_table[hash].occupied = 1;
+		}
+	}
+	*returnSize = 0;
+	return NULL;
 }
+
+int get_hash(int key) {
+	int h = key % TABLE_SIZE;
+	return (h < 0) ? (h + TABLE_SIZE) : h;
+}
+/*
+void insert(int key, int val) {
+	int idx = get_hash(key);
+	while (hash_table[idx].occupied) {
+		idx = (idx + 1) % TABLE_SIZE;
+	}
+	hash_table[idx].key = key;
+	hash_table[idx].value = val;
+	hash_table[idx].occupied = 1;
+}
+
+int find(int key) {
+	int idx = get_hash(key);
+	while (hash_table[idx].occupied) {
+		if (hash_table[idx].key == key) return hash_table[idx].value;
+		idx = (idx + 1) % TABLE_SIZE;
+	}
+	return -1;
+}
+*/
