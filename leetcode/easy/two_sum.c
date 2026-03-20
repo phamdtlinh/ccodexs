67845c09df8c8fae87e08177d706b6700175195f
Linh Pham <linharexcuulong@gmail.com>
2026-03-20 18:23:18 +0700

    finish two_sum.c

--- leetcode/easy/two_sum.c
+++ leetcode/easy/two_sum.c
@@ -12,16 +12,14 @@ typedef struct {
 Node hash_table[TABLE_SIZE] = {{0, 0, 0}};
 int *twoSum(int *nums, int numsSize, int target, int *returnSize);
 int get_hash(int key);
-/*
 void insert(int key, int val);
 int find(int key);
-*/
 
 
 int main(void) {
-	int arr[] = {2, 7, 11, 15, 2, 15};
+	int arr[] = {3,3,3};
 	int returnSize = 0;
-	int *result = twoSum(arr, 6, 9, &returnSize);
+	int *result = twoSum(arr, 3, 6, &returnSize);
 	for (int i = 0; i < returnSize; i++) {
 		printf("%d\n", result[i]);
 	}
@@ -31,26 +29,19 @@ int main(void) {
 int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
 	for (int i = 0; i < numsSize; i++) {
 		int compliment = target - nums[i];
-		int hash = get_hash(nums[i]);
-		printf("%d\n", hash);
-		printf("key: %d, value %d, occupied: %d\n", hash_table[hash].key, hash_table[hash].value, hash_table[hash].occupied);
-		if (hash_table[get_hash(compliment)].occupied == 1 ) {
+		int idx = find(compliment);
+		if (idx > -1) {
 			int *result = malloc(2 * sizeof(*result));
 			if (!result) {
 				returnSize = 0;
 				return NULL;
 			}
-			result[0] = hash_table[get_hash(compliment)].value;
+			result[0] = idx;
 			result[1] = i;
 			*returnSize = 2;
 			return result;
 		}
-		while (hash_table[hash].occupied) {
-			hash = (hash + 1) % TABLE_SIZE;
-			hash_table[hash].key = nums[i];
-			hash_table[hash].value = i;
-			hash_table[hash].occupied = 1;
-		}
+		insert(nums[i], i);
 	}
 	*returnSize = 0;
 	return NULL;
@@ -60,7 +51,7 @@ int get_hash(int key) {
 	int h = key % TABLE_SIZE;
 	return (h < 0) ? (h + TABLE_SIZE) : h;
 }
-/*
+
 void insert(int key, int val) {
 	int idx = get_hash(key);
 	while (hash_table[idx].occupied) {
@@ -79,4 +70,3 @@ int find(int key) {
 	}
 	return -1;
 }
-*/
